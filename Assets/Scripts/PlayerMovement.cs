5488a20119716d9e984008e24a08eccde41dda9b
Isaac <isaac.ebert@mohawkcollege.ca>
2025-09-17 16:31:36 -0400

    Add base player movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public int moveSpeed = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.gravityScale *= -1;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += Vector3.right * moveSpeed * Time.deltaTime;
+
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            transform.position += Vector3.right * -moveSpeed * Time.deltaTime;
+
+        }
+    }
+}
